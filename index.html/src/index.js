f0a6010de828b2da897e09e38f7b1c83b759a404
JoyMbugua <joymbugua123@gmail.com>
2024-05-15 02:56:40 +0300

    added module

--- index.html/src/index.js
+++ index.html/src/index.js
@@ -1,4 +1,4 @@
-let axios = require("axios/dist/browser/axios.cjs");
+import axios from ("axios/dist/browser/axios.cjs");
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
