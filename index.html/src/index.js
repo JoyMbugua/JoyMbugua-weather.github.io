8cef4f24f1cab479dc9aa9332b159969452277ba
JoyMbugua <joymbugua123@gmail.com>
2024-05-15 16:39:35 +0300

    axios

--- index.html/src/index.js
+++ index.html/src/index.js
@@ -1,4 +1,4 @@
-import axios from ",/axiosmodule.mjs";
+import axios from "./axiosmodule.mjs";
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
