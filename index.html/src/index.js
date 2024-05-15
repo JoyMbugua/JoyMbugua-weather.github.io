1b25a5176c1d38c5a2a72c19d3b980bd97bfd37a
JoyMbugua <joymbugua123@gmail.com>
2024-05-15 15:39:16 +0300

    fixed error

--- index.html/src/index.js
+++ index.html/src/index.js
@@ -1,4 +1,4 @@
-import axios from "axios/dist/browser/axios.cjs";
+import axios from "./axios/dist/browser/axios.cjs";
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
